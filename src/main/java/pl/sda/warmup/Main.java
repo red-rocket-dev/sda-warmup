a0f82912844881d8092ec534f80c2fd77059993e
Jakub <kubplon@gmail.com>
2020-03-06 22:37:43 +0100

    Warm up assignments

---
+++ src/main/java/pl/sda/warmup/Main.java
@@ -0,0 +1,39 @@
+package pl.sda.warmup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        1. Napisz kod, ktory wypisze w konsoli prostokąt o wymiarach 5 na 6 złożony z gwiazdek (*)
+        Kod powinno się dać łatwo zmodyfikować, żeby uzyskać inne wymiary prostokąta
+        Przydadzą się do tego:
+        Metody System.out.print i System.out.println
+        pętla for lub while
+        2. Napisany kod przenieś do metody o sygnaturze:
+        public static void printRect(int w, int h)
+        gdzie argumenty w i h to kolejno szerokość i wysokość prostokąta do narysowania
+        3. Napisz kod, który wypisze w konsoli PUSTY W ŚRODKU prostokąt o wymiarach 5 na 6 złożony z gwiazdek (obwódka)
+        i spacji w środku, dodatkowo przyda się do tego konstukcja "if"
+        4. Napisany kod przenieś do metody o sygnaturze:
+        public static void printEmptyRect(int w, int h)
+        5. Czy można uprościć w myśl DRY (don't repeat yourself)
+        Prostokąt o wymiarach 5 na 6 powinien wyglądać tak:
+
+         *****
+         *****
+         *****
+         *****
+         *****
+         *****
+        Pusty prostokąt o wymiarach 5 na 6 powinien wyglądać tak:
+
+         *****
+         *   *
+         *   *
+         *   *
+         *   *
+         *****
+
+         */
+    }
+}
